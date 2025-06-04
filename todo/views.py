dd9bb20993304bf6a14ce08efb66ef2245fcb80e
MagicExist <johhanparra1@gmail.com>
2025-06-03 19:30:04 -0500

    add api DRF to-do

---
+++ todo/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from . models import task
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = task.objects.all()
+    serializer_class = TaskSerializer
